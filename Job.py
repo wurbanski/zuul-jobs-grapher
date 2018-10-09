75ce12b049df3161960338ce86558218cd874996
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-10-09 12:24:27 +0200

    Color the graph based on type of job

--- Job.py
+++ Job.py
@@ -1,4 +1,9 @@
 class Job:
+    base_color = '#ccbe00'
+    base_fill = '#fffde0'
+    color = '#22d88c'
+    fill = '#d0fff2'
+
     def __init__(self, attributes):
         self.__dict__.update(**attributes)
         if not 'parent' in attributes.keys():
@@ -7,15 +12,23 @@ class Job:
     def __repr__(self):
         return "Job: %s, parent: %s" % (self.name, self.parent)
 
+    def __getFillcolor(self):
+        return self.base_fill if self.__isBase() else self.fill
+
+    def __getColor(self):
+        return self.base_color if self.__isBase() else self.color
+
+    def __isBase(self):
+        return 'base' in self.name
+
     def asEdge(self):
         if self.__dict__.get('parent', None):
-            return (self.name, self.parent), {}
+            return (self.name, self.parent), {'color': self.__getColor()}
         return None
 
     def asNode(self):
         return (self.name), {'label': self.name, 
-                             'fillcolor': '#fffde0' if self.isBase() else '#ffffff',
+                             'fillcolor': self.__getFillcolor(),
+                             'color': self.__getColor(),
                              'style': 'filled'}
 
-    def isBase(self):
-        return 'base' in self.name
